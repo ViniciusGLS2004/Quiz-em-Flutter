9143900974217e8423417c1792c6336951ddd97e
Vinicius Gomes Lobo <120745001+ViniciusGLS2004@users.noreply.github.com>
2023-11-27 19:06:28 -0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/flutterquiz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterquiz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\vg160\\Downloads\\flutterquiz-20231125T231703Z-001\\flutterquiz" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\vg160\\Downloads\\flutterquiz-20231125T231703Z-001\\flutterquiz"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\vg160\\Downloads\\flutterquiz-20231125T231703Z-001\\flutterquiz\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\vg160\\Downloads\\flutterquiz-20231125T231703Z-001\\flutterquiz"
+  "FLUTTER_TARGET=C:\\Users\\vg160\\Downloads\\flutterquiz-20231125T231703Z-001\\flutterquiz\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz1mYWxzZQ==,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC83NjdkOGM3NWU4OTgwOTFiOTI1NTE5ODAzODMwZmMyNzIxNjU4ZDA3Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\vg160\\Downloads\\flutterquiz-20231125T231703Z-001\\flutterquiz\\.dart_tool\\package_config.json"
+)
