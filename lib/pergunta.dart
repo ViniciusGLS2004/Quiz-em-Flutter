77ef24a56c40a5b14b44fb304e005c15828e1ddf
Vinicius Gomes Lobo <120745001+ViniciusGLS2004@users.noreply.github.com>
2023-12-03 21:03:13 -0300

    Update pergunta.dart

--- lib/pergunta.dart
+++ lib/pergunta.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
+/// Um widget reutilizável para exibir perguntas em um quiz.
 class Question extends StatelessWidget {
+  /// Texto da pergunta a ser exibido.
   final String questionText;
 
+  /// Construtor para inicializar a pergunta com o texto fornecido.
   const Question(this.questionText, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Retorna um contêiner que exibe o texto da pergunta centralizado.
     return Container(
       width: double.infinity,
       margin: const EdgeInsets.all(10),
