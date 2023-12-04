8493727dd635b80324a9213251e2b5b85d13159d
Vinicius Gomes Lobo <120745001+ViniciusGLS2004@users.noreply.github.com>
2023-12-04 06:49:51 -0300

    Update quiz.dart

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -3,11 +3,18 @@ import 'package:flutter/material.dart';
 import './resposta.dart';
 import './pergunta.dart';
 
+/// Widget para exibir um conjunto de perguntas e respostas em um quiz.
 class Quiz extends StatelessWidget {
+  /// Lista de perguntas e respostas.
   final List<Map<String, dynamic>> questions;
+
+  /// Índice da pergunta atual.
   final int questionIndex;
+
+  /// Função chamada ao responder a uma pergunta.
   final Function answerQuestion;
 
+  /// Construtor para inicializar o widget com as perguntas, índice e função de resposta.
   const Quiz({
     Key? key,
     required this.questions,
@@ -19,10 +26,12 @@ class Quiz extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        // Exibe a pergunta atual usando o widget Question.
         Question(
           questions[questionIndex]['questionText'] as String,
         ),
-        SizedBox(height: 16),
+        SizedBox(height: 16), // Espaçamento entre pergunta e respostas.
+        // Mapeia as respostas para o widget Answer.
         ...(questions[questionIndex]['answers'] as List<Map<String, dynamic>>)
             .map((answer) {
           return Padding(
